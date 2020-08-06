4ac66e20ab77a51d1bed549e7a7ed643d1fe34b3
TonySatura <tony.satura@awin.com>
2020-08-06 14:56:26 +0200

    Fixes package dependencies.

--- lib/pipeline.stack.ts
+++ lib/pipeline.stack.ts
@@ -1,4 +1,79 @@
 import * as cdk from "@aws-cdk/core";
-import * as s3 from "@aws-cdk/aws-s3";
+import * as codebuild from "@aws-cdk/aws-codebuild";
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipelineActions from "@aws-cdk/aws-codepipeline-actions";
+import { PipelineStackProps } from "./stack-props";
+import { GitHubTrigger } from "@aws-cdk/aws-codepipeline-actions";
 
-export class PipelineStack extends cdk.Stack {}
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: PipelineStackProps) {
+    super(scope, id, props);
+
+    const pipeline = new codepipeline.Pipeline(this, "pipeline", {
+      pipelineName: props.appName + "-pipeline",
+    });
+
+    //#region === SOURCE ===
+    const sourceStage = pipeline.addStage({
+      stageName: "Source",
+    });
+
+    const sourceArtifact = new codepipeline.Artifact("source");
+    const oauth = cdk.SecretValue.secretsManager("GitHubToken");
+
+    const gitHubSourceAction = new codepipelineActions.GitHubSourceAction({
+      actionName: "GitHubSource",
+      owner: props.repository.owner,
+      repo: props.repository.name,
+      output: sourceArtifact,
+      branch: props.repository.branch,
+      trigger: GitHubTrigger.WEBHOOK,
+      oauthToken: oauth,
+    });
+
+    sourceStage.addAction(gitHubSourceAction);
+    //#endregion
+
+    //#region === BUILD ===
+    const buildArtifact = new codepipeline.Artifact("build");
+
+    const buildStage = pipeline.addStage({
+      stageName: "Build",
+      placement: {
+        justAfter: sourceStage,
+      },
+    });
+
+    const codeBuildProject = new codebuild.PipelineProject(
+      this,
+      props.appName + "-codebuild"
+    );
+
+    const angularBuildAction = new codepipelineActions.CodeBuildAction({
+      actionName: "AngularBuild",
+      input: sourceArtifact,
+      outputs: [buildArtifact],
+      project: codeBuildProject,
+    });
+
+    buildStage.addAction(angularBuildAction);
+    //#endregion
+
+    //#region === DEPLOY ===
+    const deployStage = pipeline.addStage({
+      stageName: "Deploy",
+      placement: {
+        justAfter: buildStage,
+      },
+    });
+
+    const s3deploy = new codepipelineActions.S3DeployAction({
+      actionName: "S3Deploy",
+      input: buildArtifact,
+      bucket: props.siteBucket,
+    });
+
+    deployStage.addAction(s3deploy);
+    //#endregion
+  }
+}
--- lib/ui.stack.ts
+++ lib/ui.stack.ts
@@ -1,9 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
-// import * as cloudfront from "@aws-cdk/aws-cloudfront";
-// import { PriceClass } from "@aws-cdk/aws-cloudfront";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import { UiStackProps } from "./stack-props";
-import { RemovalPolicy } from "@aws-cdk/core";
 
 export class UiStack extends cdk.Stack {
   public readonly siteBucket: s3.Bucket;
@@ -15,58 +13,57 @@ export class UiStack extends cdk.Stack {
     // //     domainName: props.rootDomain
     // // });
 
-    // const siteBucket = new s3.Bucket(this, "site-", {
-    //   blockPublicAccess: {
-    //     blockPublicAcls: true,
-    //     ignorePublicAcls: true,
-    //     blockPublicPolicy: false,
-    //     restrictPublicBuckets: false,
-    //   },
-    //   websiteIndexDocument: "index.html",
-    //   publicReadAccess: true,
-    //   removalPolicy: RemovalPolicy.DESTROY,
-    // });
-    // this.siteBucket = siteBucket;
+    this.siteBucket = new s3.Bucket(this, "site-", {
+      blockPublicAccess: {
+        blockPublicAcls: true,
+        ignorePublicAcls: true,
+        blockPublicPolicy: false,
+        restrictPublicBuckets: false,
+      },
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // const siteDistribution = new cloudfront.CloudFrontWebDistribution(
-    //   this,
-    //   "site-distribution",
-    //   {
-    //     comment: "Website distribution for " + props.appName,
-    //     priceClass: PriceClass.PRICE_CLASS_ALL,
-    //     // aliasConfiguration: {
-    //     //     acmCertRef: certificateArn,
-    //     //     names: [uiSiteDomain],
-    //     //     sslMethod: cloudfront.SSLMethod.SNI,
-    //     //     securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2018
-    //     // },
-    //     originConfigs: [
-    //       {
-    //         s3OriginSource: {
-    //           s3BucketSource: siteBucket,
-    //         },
-    //         behaviors: [{ isDefaultBehavior: true }],
-    //       },
-    //     ],
-    //     errorConfigurations: [
-    //       {
-    //         errorCode: 403,
-    //         errorCachingMinTtl: 300,
-    //         responsePagePath: "/index.html",
-    //         responseCode: 200,
-    //       },
-    //       {
-    //         errorCode: 404,
-    //         errorCachingMinTtl: 300,
-    //         responsePagePath: "/index.html",
-    //         responseCode: 200,
-    //       },
-    //     ],
-    //   }
-    // );
+    const siteDistribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "site-distribution",
+      {
+        comment: "Website distribution for " + props.appName,
+        priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL,
+        // aliasConfiguration: {
+        //     acmCertRef: certificateArn,
+        //     names: [uiSiteDomain],
+        //     sslMethod: cloudfront.SSLMethod.SNI,
+        //     securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2018
+        // },
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: this.siteBucket,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+        errorConfigurations: [
+          {
+            errorCode: 403,
+            errorCachingMinTtl: 300,
+            responsePagePath: "/index.html",
+            responseCode: 200,
+          },
+          {
+            errorCode: 404,
+            errorCachingMinTtl: 300,
+            responsePagePath: "/index.html",
+            responseCode: 200,
+          },
+        ],
+      }
+    );
 
-    // new cdk.CfnOutput(this, "SiteURL", {
-    //   value: "https://" + siteDistribution.domainName,
-    // });
+    new cdk.CfnOutput(this, "SiteURL", {
+      value: "https://" + siteDistribution.domainName,
+    });
   }
 }
