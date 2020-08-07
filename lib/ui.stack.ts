5dae595137a140909a3372b6c1ec9654fd5aa844
TonySatura <tony.satura@awin.com>
2020-08-07 11:10:03 +0200

    Restrict siteBucket access to Cloudfront only.

--- lib/ui.stack.ts
+++ lib/ui.stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as iam from "@aws-cdk/aws-iam";
 import { UiStackProps } from "./stack-props";
 
 export class UiStack extends cdk.Stack {
@@ -14,23 +15,39 @@ export class UiStack extends cdk.Stack {
     // // });
 
     this.siteBucket = new s3.Bucket(this, "site-", {
-      blockPublicAccess: {
-        blockPublicAcls: true,
-        ignorePublicAcls: true,
-        blockPublicPolicy: false,
-        restrictPublicBuckets: false,
-      },
+      // blockPublicAccess: {
+      //   blockPublicAcls: true,
+      //   ignorePublicAcls: true,
+      //   blockPublicPolicy: false,
+      //   restrictPublicBuckets: false,
+      // },
       websiteIndexDocument: "index.html",
-      publicReadAccess: true,
+      // publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, "-OAI", {
+      comment: "OAI for ${props.appName} website.",
+    });
+
+    const cloudfrontS3Access = new iam.PolicyStatement();
+    cloudfrontS3Access.addActions("s3:GetBucket*");
+    cloudfrontS3Access.addActions("s3:GetObject*");
+    cloudfrontS3Access.addActions("s3:List*");
+    cloudfrontS3Access.addResources(this.siteBucket.bucketArn);
+    cloudfrontS3Access.addResources(`${this.siteBucket.bucketArn}/*`);
+    cloudfrontS3Access.addCanonicalUserPrincipal(
+      cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+    );
+
+    this.siteBucket.addToResourcePolicy(cloudfrontS3Access);
 
     const siteDistribution = new cloudfront.CloudFrontWebDistribution(
       this,
       "site-distribution",
       {
         comment: "Website distribution for " + props.appName,
-        priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL,
+        priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
         // aliasConfiguration: {
         //     acmCertRef: certificateArn,
         //     names: [uiSiteDomain],
@@ -41,24 +58,26 @@ export class UiStack extends cdk.Stack {
           {
             s3OriginSource: {
               s3BucketSource: this.siteBucket,
+              originAccessIdentity: cloudfrontOAI,
             },
             behaviors: [{ isDefaultBehavior: true }],
           },
         ],
-        errorConfigurations: [
-          {
-            errorCode: 403,
-            errorCachingMinTtl: 300,
-            responsePagePath: "/index.html",
-            responseCode: 200,
-          },
-          {
-            errorCode: 404,
-            errorCachingMinTtl: 300,
-            responsePagePath: "/index.html",
-            responseCode: 200,
-          },
-        ],
+        // === Custom errors not required without ng routing ===
+        //   errorConfigurations: [
+        //     {
+        //       errorCode: 403,
+        //       errorCachingMinTtl: 300,
+        //       responsePagePath: "/index.html",
+        //       responseCode: 200,
+        //     },
+        //     {
+        //       errorCode: 404,
+        //       errorCachingMinTtl: 300,
+        //       responsePagePath: "/index.html",
+        //       responseCode: 200,
+        //     },
+        //   ],
       }
     );
 
